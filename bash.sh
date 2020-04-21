7196edbb7b1e0cfdeaf7efd20f8123febf692ed1
LinuxSec <11514023+linuxsec@users.noreply.github.com>
2020-04-21 12:45:02 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+for sites in $(cat $1);
+do
+    echo $sites | xargs ffuf -mc all -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u $sites/FUZZ -w wordlist.txt -ac -o output.json;
+	cat output.json | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' >> result_dir.txt
+done
